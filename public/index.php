43137f2405e1f3fe757e427acc3a100cd1554018
Emmanuel Navarro <reavsetra@live.com>
2016-12-29 19:25:45 -0600

    Sbida al server

--- public/index.php
+++ public/index.php
@@ -32,8 +32,8 @@ require __DIR__.'/../bootstrap/autoload.php';
 | the responses back to the browser and delight our users.
 |
 */
-
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
 
 /*
 |--------------------------------------------------------------------------
